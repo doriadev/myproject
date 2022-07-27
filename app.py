3b8da77feb76483a1031afa5a6df054aa5e69431
doriadev <filipe.dmd2@gmail.com>
2022-07-27 00:37:01 +0000

    aula 3: fizemos os primeiros CSS

--- app.py
+++ app.py
@@ -1,11 +1,23 @@
 from flask import Flask, render_template
+from datetime import datetime
 
 app = Flask("hello2")
 
-@app.route("/")
-def hello():
-    return "Hello world"
+posts = [
+    {
+        "title": "O meu primeiraço Post",
+        "body": "Aqui é o texto do post",
+        "author": "Filipe",
+        "created": datetime(2022,7,25)
+    },
+    {
+        "title": "O meu segundo Post",
+        "body": "Aqui é o texto do post",
+        "author": "Dória",
+        "created": datetime(2022,7,26)
+    }
+]
 
-@app.route("/meucontato")
-def meuContato():
-    return render_template("index.html")
+@app.route("/")
+def index():
+    return render_template("index.html", posts=posts)
